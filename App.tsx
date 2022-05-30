5e7cfca3e6f3ea6270cf9a677eed3cd803a20765
mquenum <mikayil.quenum@gmail.com>
2022-05-30 17:19:50 +0200

    Creation of projet,

--- App.tsx
+++ App.tsx
@@ -9,3 +9,16 @@ export default function App() {
     </div>
   );
 }
+
+class className {
+  constructor() {
+    // function a toujours créer en 1er pour initialiser la class
+    this.val = 'value';
+  }
+  printKey(){
+    console.log(this.val);
+  }
+}
+// qu'on appellera ainsi, comme pour une class constructor
+const classNameName = new className();
+classNameName.printKey();
